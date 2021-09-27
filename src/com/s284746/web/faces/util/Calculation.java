46abaad5cb267116fae53d82a6573183d15233ab
AXER-doc <cool.ball2901@gmail.com>
2021-09-27 13:07:37 +0300

    build 3.0.1

--- src/com/s284746/web/faces/util/Calculation.java
+++ src/com/s284746/web/faces/util/Calculation.java
@@ -2,13 +2,13 @@ package com.s284746.web.faces.util;
 
 public class Calculation {
     private static boolean belongsToRectangle(double x, double y, double r) {
-        return x >= 0 && x < r && y > -r/2 && y <= 0;
+        return x >= 0 && x < r/2 && y > -r && y <= 0;
     }
     private static boolean belongsToSector(double x, double y, double r) {
-        return x <= 0 && y >= 0 && x*x + y*y < r*r/4;
+        return x >= 0 && y >= 0 && x*x + y*y < r*r/4;
     }
     private static boolean belongsToTriangle(double x, double y, double r) {
-        return x >= 0 && y >= 0 && x + 2*y < r;
+        return x <= 0 && y >= 0 && y - x <= r;
     }
     public static boolean belongsToArea(double x, double y, double r) {
         if (r > 0) return belongsToRectangle(x, y, r) || belongsToSector(x, y, r) || belongsToTriangle(x, y, r);
