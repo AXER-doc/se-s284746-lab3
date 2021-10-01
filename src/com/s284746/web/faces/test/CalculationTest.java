3725dc8a2f9aaaf442a72d31c784448c1c5e0fa0
Asus <cool.ball2901@gmail.com>
2021-10-01 18:45:39 +0300

    build 3.0.3

---
+++ src/com/s284746/web/faces/test/CalculationTest.java
@@ -0,0 +1,12 @@
+package com.s284746.web.faces.test;
+
+import com.s284746.web.faces.util.Calculation;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class CalculationTest {
+    @Test
+    public void testFunction() {
+        // Assert.assertEquals(true, Calculation.belongsToArea(-2, 0, 2));
+    }
+}
