b0f040122fb31de25b32c8801d69d8b504dfbc89
AXER-doc <cool.ball2901@gmail.com>
2021-10-16 10:37:21 +0300

    build 3.1.2

--- src/main/java/com/s284746/web/faces/event/DataDeleter.java
+++ src/main/java/com/s284746/web/faces/event/DataDeleter.java
@@ -5,11 +5,16 @@ import javax.faces.event.*;
 import com.s284746.web.faces.util.SQLProcessor;
 
 /**
- * todo comment
+ * Class for performing data deletion when 'Clear' button is pressed
  */
 public class DataDeleter implements ActionListener {
 	public static int userid = 0;
 
+	/**
+	 * Catches a click and invokes process
+	 * @param actionEvent click event as object
+	 * @throws AbortProcessingException if something is wrong
+	 */
 	@Override
 	public void processAction(ActionEvent actionEvent) throws AbortProcessingException {
 		try {
